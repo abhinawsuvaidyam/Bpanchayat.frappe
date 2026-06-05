a164992ba40ade36054e2d2143cbc148bcfe2eb3
Abhinaw Pratap Singh <abhinaw.suvaidyam@gmail.com>
2026-06-05 06:38:03 +0000

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='bpanchayat',
+    version='0.0.1',
+    description='About Bihar Panchayat',
+    author='Abhinaw',
+    author_email='Bpanchayat@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
+ 
